3179e2b382ee33fd2f5470d26ff946d9ed28c08a
Maxim Berdyshev <36857730+maximberdyshev@users.noreply.github.com>
2023-05-31 21:31:43 +0500

    added: strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { App } from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+const root = createRoot(document.getElementById('root'))
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
